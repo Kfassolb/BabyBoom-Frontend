1f550b1829bb9f5310ff4a26ef1b1a47061d367c
SheylaQuispe <106836940+SheylaQuispe@users.noreply.github.com>
2023-06-21 08:25:38 -0500

    guarderia creadita

--- BabyBoom/src/app/components/guarderiaservicio/guarderiaservicio-creadita/guarderiaservicio-creadita.component.ts
+++ BabyBoom/src/app/components/guarderiaservicio/guarderiaservicio-creadita/guarderiaservicio-creadita.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GuarderiaServicio } from 'src/app/model/GuarderiaServicio';
+import { Servicio } from 'src/app/model/Servicio';
+import { GuarderiaservicioService } from 'src/app/service/guarderiaservicio.service';
+import { ServicioService } from 'src/app/service/servicio.service';
 
 @Component({
   selector: 'app-guarderiaservicio-creadita',
   templateUrl: './guarderiaservicio-creadita.component.html',
   styleUrls: ['./guarderiaservicio-creadita.component.css']
 })
-export class GuarderiaservicioCreaditaComponent {
+export class GuarderiaservicioCreaditaComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  guarderiaservicio: GuarderiaServicio = new GuarderiaServicio()
+  mensaje: string = ""
+  //maxFecha: Date = moment().add(-1, 'days').toDate();
+  lista: Servicio[] = [];
+  idServicioSeleccionado: number = 0;
 
+
+  constructor(private GSs: GuarderiaservicioService,
+    private router: Router,
+    private route: ActivatedRoute, private Ss:ServicioService) {
+  }
+  ngOnInit(): void {
+    this.Ss.list().subscribe(data => { this.lista = data });
+
+    this.form = new FormGroup({
+      idGuarderiaServicio: new FormControl(),
+      Servicio :new FormControl()
+    });
+
+  }
+  aceptar(): void {
+    this.guarderiaservicio.idGuarderiaServicio = this.form.value['idGuarderiaServicio'];
+    this.guarderiaservicio.servicio.NombreServicio=this.form.value['servicio.NombreServicio'];
+    if (this.idServicioSeleccionado>0) {
+      let s = new Servicio();
+      s.id = this.idServicioSeleccionado;
+      this.guarderiaservicio.servicio=s;
+      this.GSs.insert(this.guarderiaservicio).subscribe(() => {
+      this.GSs.list().subscribe(data => {
+            this.GSs.setList(data);
+          })
+        })
+      this.router.navigate(['guarderiaservicio']);
+  }
+}
 }
